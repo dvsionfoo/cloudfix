556330fe635bfa96ee7a6dec706bf964ef1b4410
Atul Pandey <atulkp.eee13@nituk.ac.in>
2021-02-24 10:25:31 +0530

    Add filter and sort options to the tables/grids

--- src/app/components/CompletedRecommendations.js
+++ src/app/components/CompletedRecommendations.js
@@ -1,64 +1,115 @@
-import React from "react";
-import { Badge } from "react-bootstrap";
+import React, {useState} from "react";
+import {Badge} from "react-bootstrap";
 
-import { RecommendationActions } from './RecommendationActions';
-import { FormatNumber } from "./../helpers";
+import {RecommendationActions} from './RecommendationActions';
+import {FormatNumber} from "./../helpers";
+import {filterRecommendationData, sortColumnData} from './../helpers/SortingAndFilterHelpers'
 
-export const CompletedRecommendations = ({ recommendations, onActionClick }) => {
+export const CompletedRecommendations = ({recommendations, onActionClick}) => {
 
-  const handleOnAction = (recommendationId, actionType = 'now') => {
-    onActionClick([recommendationId], actionType);
-  };
+    const [tableData, setTableData] = useState(recommendations);
 
-  return (
-    <>
+    const handleOnAction = (recommendationId, actionType = 'now') => {
+        onActionClick([recommendationId], actionType);
+    };
 
-      {
-        (!recommendations || recommendations.length === 0) ? (
-          <p>No fixes! Go to new recommendations tab to get started.</p>
-        ) : (
-            <div className="table-responsive">
-              <table
-                className="table table-head-custom table-vertical-center"
-                id="kt_advance_table_widget_1"
-              >
-                <thead>
-                  <tr className="text-left">
-                    <th>RECOMMENDATION</th>
-                    <th>RESOURCE ID</th>
-                    <th>ACCOUNT</th>
-                    <th>REGION</th>
-                    <th>SAVINGS</th>
-                    <th>STATUS</th>
-                    <th>ACTIONS</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {recommendations.map((recommendation, index) => (
-                    <tr key={index}>
-                      <td>{recommendation.opportunityDescription}</td>
-                      <td>{recommendation.resourceId}</td>
-                      <td>{recommendation.accountId}</td>
-                      <td>{recommendation.region}</td>
-                      <td>${FormatNumber(recommendation.annualSavings)}</td>
-                      <td><Badge pill variant="primary">{recommendation.status}</Badge></td>
-                      <td> <div style={{ width: "120px" }}>
-                        <RecommendationActions
-                          recommendationId={recommendation.id}
-                          onAction={handleOnAction}
-                          recommendationType="Completed"
-                          lastExecutionDate={recommendation.scheduledAt}
-                        />
-                      </div>
-                      </td>
-                    </tr>
-                  ))}
+    const sortColumn = (sortColumn, sortType) => {
+        setTableData(sortColumnData(sortColumn, sortType, tableData));
+    };
 
-                </tbody>
-              </table>
-            </div >
-          )
-      }
-    </>
-  );
+    const filterColumn = (event, filterOn) => {
+        setTableData(filterRecommendationData(filterOn, event, recommendations));
+    };
+
+    return (
+        <>
+
+            {
+                (!tableData || tableData.length === 0) ? (
+                    <p>No fixes! Go to new recommendations tab to get started.</p>
+                ) : (
+                    <div className="table-responsive">
+                        <table
+                            className="table table-head-custom table-vertical-center"
+                            id="kt_advance_table_widget_1"
+                        >
+                            <thead>
+                            <tr className="text-left">
+                                <th>RECOMMENDATION</th>
+                                <th>{<>
+                                    <div className="sortArrowContainer">RESOURCE ID
+                                        <div className="sortArrows">
+                                            <img src={"/media/up-arrow.svg"}
+                                                 onClick={() => sortColumn('resourceId', 'asc')} alt="icon"/>
+                                            <img src={"/media/down-arrow.svg"}
+                                                 onClick={() => sortColumn('resourceId', 'desc')}
+                                                 alt="icon"/>
+                                        </div>
+                                    </div>
+                                    <input onChange={(event) => filterColumn(event, 'resourceId')}
+                                           placeholder="Search Resource Id"/></>}</th>
+                                <th>{
+                                    <>
+                                        <div className="sortArrowContainer">ACCOUNT
+                                            <div className="sortArrows">
+                                                <img src={"/media/up-arrow.svg"}
+                                                     onClick={() => sortColumn('accountId', 'asc')}
+                                                     alt="icon"/>
+                                                <img src={"/media/down-arrow.svg"}
+                                                     onClick={() => sortColumn('accountId', 'desc')}
+                                                     alt="icon"/>
+                                            </div>
+                                        </div>
+                                        <input onChange={(event) => filterColumn(event, 'accountId')}
+                                               placeholder="Search Account"/></>
+                                }</th>
+                                <th>{
+                                    <>
+                                        <div className="sortArrowContainer">REGION
+                                            <div className="sortArrows">
+                                                <img src={"/media/up-arrow.svg"}
+                                                     onClick={() => sortColumn('region', 'asc')}
+                                                     alt="icon"/>
+                                                <img src={"/media/down-arrow.svg"}
+                                                     onClick={() => sortColumn('region', 'desc')}
+                                                     alt="icon"/>
+                                            </div>
+                                        </div>
+                                        <input onChange={(event) => filterColumn(event, 'region')}
+                                               placeholder="Search Region"/></>
+                                }</th>
+                                <th>SAVINGS</th>
+                                <th>STATUS</th>
+                                <th>ACTIONS</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {tableData.map((recommendation, index) => (
+                                <tr key={index}>
+                                    <td>{recommendation.opportunityDescription}</td>
+                                    <td>{recommendation.resourceId}</td>
+                                    <td>{recommendation.accountId}</td>
+                                    <td>{recommendation.region}</td>
+                                    <td>${FormatNumber(recommendation.annualSavings)}</td>
+                                    <td><Badge pill variant="primary">{recommendation.status}</Badge></td>
+                                    <td>
+                                        <div style={{width: "120px"}}>
+                                            <RecommendationActions
+                                                recommendationId={recommendation.id}
+                                                onAction={handleOnAction}
+                                                recommendationType="Completed"
+                                                lastExecutionDate={recommendation.scheduledAt}
+                                            />
+                                        </div>
+                                    </td>
+                                </tr>
+                            ))}
+
+                            </tbody>
+                        </table>
+                    </div>
+                )
+            }
+        </>
+    );
 };
--- src/app/components/NewRecommendations.js
+++ src/app/components/NewRecommendations.js
@@ -1,16 +1,17 @@
-import React, { useState } from "react";
-import { Button} from "react-bootstrap";
+import React, {useState} from "react";
+import {Button} from "react-bootstrap";
 
-import { toAbsoluteUrl, FormatNumber } from "./../helpers";
-import { RecommendationActions } from './RecommendationActions';
+import {FormatNumber, toAbsoluteUrl} from "./../helpers";
+import {RecommendationActions} from './RecommendationActions';
+import {filterRecommendationData, sortColumnData} from './../helpers/SortingAndFilterHelpers'
 
-export const NewRecommendations = ({ recommendations, onActionClick }) => {
- 
+export const NewRecommendations = ({recommendations, onActionClick}) => {
+
     const [allChecked, setAllchecked] = useState(false);
     const [selectedRecommendations, setSelectedRecommendations] = useState([]);
-
+    const [tableData, setTableData] = useState(recommendations);
     const toggleAllCheckboxes = () => {
-        if (!allChecked) { 
+        if (!allChecked) {
             const allRecommendations = recommendations.map(r => r.id);
             setSelectedRecommendations(allRecommendations);
             setAllchecked(true);
@@ -26,11 +27,11 @@ export const NewRecommendations = ({ recommendations, onActionClick }) => {
 
     const addRecommendation = (recommendationId) => {
         let newRecommendations = [...selectedRecommendations, recommendationId];
-        setSelectedRecommendations(newRecommendations);        
+        setSelectedRecommendations(newRecommendations);
     }
 
     const removeRecommendation = (recommendationId) => {
-        let newRecommendations = selectedRecommendations.filter(r => r !== recommendationId);          
+        let newRecommendations = selectedRecommendations.filter(r => r !== recommendationId);
         setSelectedRecommendations(newRecommendations);
     }
 
@@ -45,106 +46,157 @@ export const NewRecommendations = ({ recommendations, onActionClick }) => {
     const handleBulkAction = (actionType) => () => {
         setAllchecked(false);
         setSelectedRecommendations([]);
-        onActionClick(selectedRecommendations, actionType);        
+        onActionClick(selectedRecommendations, actionType);
     }
+
+
+    const sortColumn = (sortColumn, sortType) => {
+        setTableData(sortColumnData(sortColumn, sortType, tableData));
+    };
+
+    const filterColumn = (event, filterOn) => {
+        setTableData(filterRecommendationData(filterOn, event, recommendations));
+    };
 
     return (
         <>
-            
             {
-                (!recommendations || recommendations.length === 0) ? (
-                    <p>No new recommendations yet! We'll email you as soon as we find new saving opportunities.</p>                        
+                (!tableData || tableData.length === 0) ? (
+                    <p>No new recommendations yet! We'll email you as soon as we find new saving opportunities.</p>
                 ) : (
-                        <div className="table-responsive">
-                            <table
-                                className="table table-head-custom table-vertical-center"
-                                id="kt_advance_table_widget_1"
-                            >
-                                <thead>
-                                    <tr className="text-left">
-                                        <th>
-                                            <label className="checkbox checkbox-sm checkbox-single">
-                                                <input type="checkbox" onClick={toggleAllCheckboxes} checked={allChecked} />
-                                                <span></span>
-                                            </label>
-                                        </th>
-                                        
-                                            {
-                                              selectedRecommendations.length > 0 ? (
-                                                <th className="bulkActions" colSpan="6">
-                                                    <div style={{ width: "500px" }}>
-                                                        <Button variant="outline-secondary" onClick={handleBulkAction('now')}>
-                                                            <img 
-                                                                alt="action" 
-                                                                className="actionLink" 
-                                                                src={toAbsoluteUrl("/media/run.png")} 
-                                                            /> 
-                                                            <span>RUN NOW</span>
-                                                        </Button>
-                                                        <Button variant="outline-secondary" onClick={handleBulkAction('schedule')}>
-                                                            <img 
-                                                                alt="action" 
-                                                                className="actionLink" 
-                                                                src={toAbsoluteUrl("/media/schedule.png")} 
-                                                            /> 
-                                                            <span>SCHEDULE</span>
-                                                        </Button>
-                                                        <Button variant="outline-secondary" onClick={handleBulkAction('cancel')}>
-                                                            <img 
-                                                                alt="action" 
-                                                                className="actionLink" 
-                                                                src={toAbsoluteUrl("/media/delete.png")}
-                                                            /> 
-                                                            <span>IGNORE</span>
-                                                        </Button>
-                                                    </div>
-                                                </th>
-                                              ) :  (
-                                                <React.Fragment>
-                                                    <th>RECOMMENDATION</th>
-                                                    <th>RESOURCE ID</th>
-                                                    <th>ACCOUNT</th>
-                                                    <th>REGION</th>
-                                                    <th>SAVINGS</th>
-                                                    <th>ACTIONS</th>
-                                                </React.Fragment>
-                                              ) 
-                                        }                                        
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {recommendations.map((recommendation, index) => (
-                                        <tr key={index}>
-                                            <td>
-                                                <label className="checkbox checkbox-sm checkbox-single">
-                                                    <input
-                                                        type="checkbox"
-                                                        checked={allChecked || isRecommendationSelected(recommendation.id)}
-                                                        onClick={addRemoveRecommendation(recommendation.id)} />
-                                                    <span></span>
-                                                </label>
-                                            </td>
-                                            <td>{recommendation.opportunityDescription}</td>
-                                            <td>{recommendation.resourceId}</td>
-                                            <td>{recommendation.accountId}</td>
-                                            <td>{recommendation.region}</td>
-                                            <td>${FormatNumber(recommendation.annualSavings)}</td>
-                                            <td> <div style={{ width: "120px" }}>                                             
-                                                <RecommendationActions
-                                                    recommendationId={recommendation.id}
-                                                    onAction={handleOnAction}
-                                                    recommendationType="New"
-                                                    lastExecutionDate={recommendation.scheduledAt}
-                                                />
-                                                </div>  
-                                            </td>
-                                        </tr>
-                                    ))}
+                    <div className="table-responsive">
+                        <table
+                            className="table table-head-custom table-vertical-center"
+                            id="kt_advance_table_widget_1"
+                        >
+                            <thead>
+                            <tr className="text-left">
+                                <th>
+                                    <label className="checkbox checkbox-sm checkbox-single">
+                                        <input type="checkbox" onClick={toggleAllCheckboxes} checked={allChecked}/>
+                                        <span></span>
+                                    </label>
+                                </th>
 
-                                </tbody>
-                            </table>
-                        </div >
-                    )
+                                {
+                                    selectedRecommendations.length > 0 ? (
+                                        <th className="bulkActions" colSpan="6">
+                                            <div style={{width: "500px"}}>
+                                                <Button variant="outline-secondary" onClick={handleBulkAction('now')}>
+                                                    <img
+                                                        alt="action"
+                                                        className="actionLink"
+                                                        src={toAbsoluteUrl("/media/run.png")}
+                                                    />
+                                                    <span>RUN NOW</span>
+                                                </Button>
+                                                <Button variant="outline-secondary"
+                                                        onClick={handleBulkAction('schedule')}>
+                                                    <img
+                                                        alt="action"
+                                                        className="actionLink"
+                                                        src={toAbsoluteUrl("/media/schedule.png")}
+                                                    />
+                                                    <span>SCHEDULE</span>
+                                                </Button>
+                                                <Button variant="outline-secondary"
+                                                        onClick={handleBulkAction('cancel')}>
+                                                    <img
+                                                        alt="action"
+                                                        className="actionLink"
+                                                        src={toAbsoluteUrl("/media/delete.png")}
+                                                    />
+                                                    <span>IGNORE</span>
+                                                </Button>
+                                            </div>
+                                        </th>
+                                    ) : (
+                                        <React.Fragment>
+                                            <th>RECOMMENDATION</th>
+                                            <th>{<>
+                                                <div className="sortArrowContainer">RESOURCE ID
+                                                    <div className="sortArrows">
+                                                        <img src={"/media/up-arrow.svg"}
+                                                             onClick={() => sortColumn('resourceId', 'asc')}
+                                                             alt="icon"/>
+                                                        <img src={"/media/down-arrow.svg"}
+                                                             onClick={() => sortColumn('resourceId', 'desc')}
+                                                             alt="icon"/>
+                                                    </div>
+                                                </div>
+                                                <input onChange={(event) => filterColumn(event, 'resourceId')}
+                                                       placeholder="Search Resource Id"/></>}</th>
+                                            <th>{
+                                                <>
+                                                    <div className="sortArrowContainer">ACCOUNT
+                                                        <div className="sortArrows">
+                                                            <img src={"/media/up-arrow.svg"}
+                                                                 onClick={() => sortColumn('accountId', 'asc')}
+                                                                 alt="icon"/>
+                                                            <img src={"/media/down-arrow.svg"}
+                                                                 onClick={() => sortColumn('accountId', 'desc')}
+                                                                 alt="icon"/>
+                                                        </div>
+                                                    </div>
+                                                    <input onChange={(event) => filterColumn(event, 'accountId')}
+                                                           placeholder="Search Account"/></>
+                                            }</th>
+                                            <th>{
+                                                <>
+                                                    <div className="sortArrowContainer">REGION
+                                                        <div className="sortArrows">
+                                                            <img src={"/media/up-arrow.svg"}
+                                                                 onClick={() => sortColumn('region', 'asc')}
+                                                                 alt="icon"/>
+                                                            <img src={"/media/down-arrow.svg"}
+                                                                 onClick={() => sortColumn('region', 'desc')}
+                                                                 alt="icon"/>
+                                                        </div>
+                                                    </div>
+                                                    <input onChange={(event) => filterColumn(event, 'region')}
+                                                           placeholder="Search Region"/></>
+                                            }</th>
+                                            <th>SAVINGS</th>
+                                            <th>ACTIONS</th>
+                                        </React.Fragment>
+                                    )
+                                }
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {tableData.map((recommendation, index) => (
+                                <tr key={index}>
+                                    <td>
+                                        <label className="checkbox checkbox-sm checkbox-single">
+                                            <input
+                                                type="checkbox"
+                                                checked={allChecked || isRecommendationSelected(recommendation.id)}
+                                                onClick={addRemoveRecommendation(recommendation.id)}/>
+                                            <span></span>
+                                        </label>
+                                    </td>
+                                    <td>{recommendation.opportunityDescription}</td>
+                                    <td>{recommendation.resourceId}</td>
+                                    <td>{recommendation.accountId}</td>
+                                    <td>{recommendation.region}</td>
+                                    <td>${FormatNumber(recommendation.annualSavings)}</td>
+                                    <td>
+                                        <div style={{width: "120px"}}>
+                                            <RecommendationActions
+                                                recommendationId={recommendation.id}
+                                                onAction={handleOnAction}
+                                                recommendationType="New"
+                                                lastExecutionDate={recommendation.scheduledAt}
+                                            />
+                                        </div>
+                                    </td>
+                                </tr>
+                            ))}
+
+                            </tbody>
+                        </table>
+                    </div>
+                )
             }
         </>
     );
